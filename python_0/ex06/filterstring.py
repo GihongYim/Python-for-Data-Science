5d82b708549a657e3e5ab8218de48f2597c33fda
Gihong Yim <gyim@c2r8s1.42seoul.kr>
2023-10-11 14:47:01 +0900

    python_0 ex06 ex07

--- python_0/ex06/filterstring.py
+++ python_0/ex06/filterstring.py
@@ -1,8 +1,23 @@
 import sys
+from ft_filter import ft_filter
+
 
 def main():
     # your test and your error handeling
-    pass
+    try:
+        if len(sys.argv) != 3:
+            raise AssertionError("the arguments are bad")
+        if sys.argv[1].isdigit() is not False:
+            raise AssertionError("the arguments are bad")
+        words = sys.argv[1].split()
+        for word in words:
+            if word.isalpha() is False:
+                raise AssertionError("the arguments are bad")
+        result = ft_filter(lambda x: len(x) >= int(sys.argv[2]), words)
+        print(result)
+    except Exception as e:
+        print("AssertionError: ", e)
+
 
 if __name__ == "__main__":
     main()
---
+++ python_0/ex07/sos.py
@@ -0,0 +1,67 @@
+import sys
+
+
+def main():
+    # your tests and your error handling
+    NESTED_MORSE = {
+        " ": "/ ",
+        "A": ".- ",
+        'B': '-...',
+        'C': '-.-.',
+        'D': '-..',
+        'E': '.',
+        'F': '..-.',
+        'G': '--.',
+        'H': '....',
+        'I': '..',
+        'J': '.---',
+        'K': '-.-',
+        'L': '.-..',
+        'M': '--',
+        'N': '-.',
+        'O': '---',
+        'P': '.--.',
+        'Q': '--.-',
+        'R': '.-.',
+        'S': '...',
+        'T': '-',
+        'U': '..-',
+        'V': '...-',
+        'W': '.--',
+        'X': '-..-',
+        'Y': '-.--',
+        'Z': '--..',
+        '1': '.----',
+        '2': '..---',
+        '3': '...--',
+        '4': '....-',
+        '5': '.....',
+        '6': '-....',
+        '7': '--...',
+        '8': '---..',
+        '9': '----.',
+        '0': '-----',
+        ', ': '--..--',
+        '.': '.-.-.-',
+        '?': '..--..',
+        '/': '-..-.',
+        '-': '-....-',
+        '(': '-.--.',
+        ')': '-.--.-'
+    }
+    char_list = []
+    if len(sys.argv) != 2:
+        raise AssertionError("the arguments are bad")
+    for char in sys.argv[1]:
+        if char.upper() not in NESTED_MORSE.keys():
+            raise AssertionError("the arguments are bad")
+        char_list.append(NESTED_MORSE[char.upper()])
+    encoded = " ".join(char_list)
+    print(encoded)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        print("AssertionError: ", e)
