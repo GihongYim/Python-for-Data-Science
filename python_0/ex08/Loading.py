46b0bb7ed9f5315122ee85eb4f6ccd454e50820b
Gihong Yim <gyim@c2r8s1.42seoul.kr>
2023-10-11 20:54:34 +0900

    tqpm process bar

---
+++ python_0/ex08/Loading.py
@@ -0,0 +1,17 @@
+from time import time
+import os
+def ft_tqdm(total):
+    # print(os.get_terminal_size())
+    start_time = time()
+    line_length = os.get_terminal_size().columns - 40
+    for i in total:
+        percent = (i + 1) * 100 // len(total)
+        percent_str = f'{percent:3}%'
+        bar = "█" * ((i + 1) * (line_length ) // len(total))
+        space = " " * (line_length - len(bar))
+        process_bar = f'|{bar + space}|'
+        curr_over_total = f'{i}/{len(total)}'
+        loading_time = f'00:00'
+        it_per_s = f'{(i + 1) / (time() - start_time):.2f}it/s'
+        print(f"\r{percent_str}{process_bar} {curr_over_total} [{loading_time}<{loading_time}, {it_per_s}]", end="")
+        yield i
