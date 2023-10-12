8a7004d593a789dc721f618c55d29fbb3e1b5c79
Gihong Yim <gyim@c2r8s1.42seoul.kr>
2023-10-12 15:12:05 +0900

    python_0 ex09 not started git push

--- python_0/ex08/Loading_test.py
+++ python_0/ex08/Loading_test.py
@@ -1,7 +1,9 @@
-
+from time import time
+import os
 def ft_tqdm(total):
     # print(os.get_terminal_size())
-    line_length = 109
+    start_time = time()
+    line_length = os.get_terminal_size().columns - 40
     for i in total:
         percent = (i + 1) * 100 // len(total)
         percent_str = f'{percent:3}%'
@@ -9,5 +11,7 @@ def ft_tqdm(total):
         space = " " * (line_length - len(bar))
         process_bar = f'|{bar + space}|'
         curr_over_total = f'{i}/{len(total)}'
-        print(f"\r{percent_str}{process_bar} {curr_over_total}", end="")
+        elapsed_time = f'{time() - start_time:2.2}'
+        it_per_s = f'{(i + 1) / (time() - start_time):.2f}it/s'
+        print(f"\r{percent_str}{process_bar} {curr_over_total} [{elapsed_time}<{elapsed_time}, {it_per_s}]", end="")
         yield i
