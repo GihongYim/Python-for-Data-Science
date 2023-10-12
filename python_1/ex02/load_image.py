fbb3f458f921e5d57e856798bd702eaa942c86e4
Gihong Yim <gyim@c2r8s1.42seoul.kr>
2023-10-12 18:11:53 +0900

    python1 ex02 ex03

---
+++ python_1/ex02/load_image.py
@@ -0,0 +1,16 @@
+import numpy as np
+from PIL import Image
+
+
+def ft_load(filename):
+    img = Image.open(filename)
+    x = np.array(img)
+    print(f'The shape of image is: {x.shape}')
+    print(x)
+
+
+def main():
+    load_image("landscape.jpg")
+
+if __name__ == "__main__":
+    main()
