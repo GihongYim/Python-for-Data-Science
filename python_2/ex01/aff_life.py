ed5bb7b6a2ffebd69e2019c64d2ac534ed0bacaa
Gihong Yim <gyim@c2r8s1.42seoul.kr>
2023-10-13 21:11:45 +0900

    python_2_ex01 finished

--- python_2/ex01/aff_life.py
+++ python_2/ex01/aff_life.py
@@ -6,11 +6,12 @@ import matplotlib.pyplot as plt
 def main():
     data = load("life_expectancy_years.csv")
 
-    print(data)
-    # sns.lineplot(x=data.columns, y=data.loc['South Korea'], data=data)
-    # plt.title('South Korea expectancy Projections')
+    country_data = data.loc['South Korea']
+    print(country_data)
+    sns.lineplot(data=country_data)
+    plt.title('South Korea expectancy Projections')
 
-    # plt.show()
+    plt.show()
 
 if __name__ == "__main__":
     main()
