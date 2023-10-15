6d2e7b48dba0ec342f226368f2caad070bfb99f5
Gihong Yim <gyim@c2r8s1.42seoul.kr>
2023-10-15 17:59:37 +0900

    python_2 ex03 finish

---
+++ python_2/ex03/projection_life.py
@@ -0,0 +1,25 @@
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from load_csv import load
+
+def change_suffix(x):
+    x = x.replace('M', 'e+06')
+    x = x.replace('k', 'e+03')
+    return x
+
+
+def main():
+    income_data = load("income_per_person_gdppercapita_ppp_inflation_adjusted.csv")
+    income_data = income_data.set_index('country')
+    print(income_data['1900'])
+    year_income_data = income_data['1900']
+    pop_data = load("life_expectancy_years.csv")
+    pop_data = pop_data.set_index('country')
+    print(pop_data['1900'])
+    year_pop_data = pop_data['1900']
+    sns.scatterplot(x=year_income_data, y=year_pop_data)
+    plt.xscale('log')
+    plt.show()
+if __name__ == "__main__":
+    main()
