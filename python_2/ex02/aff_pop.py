ab7e5b9c41df2fa07349bb6eec9fddcf0e3ac643
Gihong Yim <gyim@c2r8s1.42seoul.kr>
2023-10-15 13:05:53 +0900

    python_2 ex02 load_csv changed

---
+++ python_2/ex02/aff_pop.py
@@ -0,0 +1,20 @@
+from load_csv import load
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def main():
+    data = load("population_total.csv")
+    print(data)
+    data = data.set_index('country')
+    country_data = data.loc['South Korea']
+    country_data.index = country_data.index.astype(np.int64)
+    print(type(country_data))
+    sns.lineplot(data=country_data)
+    plt.title('South Korea expectancy Projections')
+
+    plt.show()
+
+if __name__ == "__main__":
+    main()
