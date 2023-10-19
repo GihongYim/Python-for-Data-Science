0a2ae826904ec345f625e5f66e8a4127b4f1f427
Gihong Yim <gyim@c2r8s1.42seoul.kr>
2023-10-19 14:01:01 +0900

    python_4 ex03

--- python_4/ex03/new_student.py
+++ python_4/ex03/new_student.py
@@ -5,7 +5,28 @@ from dataclasses import dataclass, field
 
 def generate_id() -> str:
     return "".join(random.choices(string.ascii_lowercase, k = 15))
-@dataclass
+
+
+@dataclass(kw_only=True)
 class Student:
-    # your code here
+    """Representing Student Class"""
+    name: str
+    surname: str
+    active: bool
+    login: str
+    id: str
+    
+    def __init__(self, name: str, surname: str, active: bool = True):
+        self.name = name
+        self.surname = surname
+        self.active = active
+        self.login = name[0] + surname
+        self.id = generate_id()
+    
+
+def main():
     pass
+   
+
+if __name__ == "__main__":
+    main()
--- python_4/ex03/tester.py
+++ python_4/ex03/tester.py
@@ -1,5 +1,13 @@
-from new_student import Student
+# from new_student import Student
 
 
-student = Student(name = "Edward", surname = "agle")
-print(student)
+# student = Student(name="Edward", surname="agle")
+
+# print(student)
+
+
+# from new_student import Student
+
+
+# student = Student(name="Edward", surname="agle", id="toto")
+# print(student)
