ee26e47c0541d63887b2c54f8161abc24a4dd5f3
Gihong Yim <gyim@c2r8s1.42seoul.kr>
2023-10-24 13:17:47 +0900

    imshow cmap option added

--- python_1/ex03/zoom.py
+++ python_1/ex03/zoom.py
@@ -2,25 +2,43 @@ from load_image import ft_load
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def zoom():
-    img = ft_load("animal.jpeg").convert('L')
+    """
+        Args:
+            None
+        Returns:
+            None
+        load the image "animal.jpeg"
+        print information about it
+        diaplay it after zooming
+    """
+    img = ft_load("animal.jpeg")
     array = np.array(img)
-    w, h = img.size
-    zoom_size = 400
-    zoom_img = img.crop((w / 2 - zoom_size / 2, h / 2 -zoom_size / 2, w / 2 + zoom_size / 2, h / 2 + zoom_size / 2))
     print(f'The shape of image is: {array.shape}')
-    array = np.array(img)
     print(array)
+    gray_img = img.convert('L')
+    gray_array = np.array(gray_img)
+    w, h = gray_array.shape
+    zoom_size = 400
+    zoom_img = gray_img.crop(
+        (w / 2 - zoom_size / 2,
+         h / 2 - zoom_size / 2,
+         w / 2 + zoom_size / 2,
+         h / 2 + zoom_size / 2)
+    )
     print('New shape after slicing: (400, 400, 1) or (400 ,400)')
     zoom_array = np.array(zoom_img)
     print(zoom_array)
-    fsig = plt.figure()
-    plt.imshow(zoom_array)
+    plt.figure()
+    plt.imshow(zoom_array, cmap='gray')
     plt.show()
 
 
 def main():
+    """main function"""
     zoom()
+
 
 if __name__ == '__main__':
     main()
