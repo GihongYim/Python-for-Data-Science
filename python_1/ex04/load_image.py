3a556a8436efd083c45fb2d0873280157e58a943
Gihong Yim <gyim@c2r8s1.42seoul.kr>
2023-10-24 13:37:05 +0900

    cmap='gray' added

--- python_1/ex04/load_image.py
+++ python_1/ex04/load_image.py
@@ -3,12 +3,28 @@ from PIL import Image
 
 
 def ft_load(filename):
+    """_summary_
+        load image and print RGB value for each pixel
+    Args:
+        filename (_type_): valid for Image.open() function
+    Return:
+        img: np.array
+    """
+
     img = Image.open(filename)
+    x = np.array(img)
+    print(f'The shape of image is: {x.shape}')
+    print(x)
     return img
 
 
 def main():
+    """
+    _summary_
+    test function for ft_load
+    """
     ft_load("animal.jpeg")
+
 
 if __name__ == "__main__":
     main()
--- python_1/ex04/rotate.py
+++ python_1/ex04/rotate.py
@@ -2,23 +2,36 @@ from load_image import ft_load
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 def rotate():
+    """
+        rotate image animal.jpeg
+    """
     img = ft_load("animal.jpeg").convert('L')
     w, h = img.size
     zoom_size = 400
-    zoom_img = img.crop((w / 2 - zoom_size / 2, h / 2 -zoom_size / 2, w / 2 + zoom_size / 2, h / 2 + zoom_size / 2))
+    zoom_img = img.crop((
+        w / 2 - zoom_size / 2,
+        h / 2 - zoom_size / 2,
+        w / 2 + zoom_size / 2,
+        h / 2 + zoom_size / 2
+        ))
     zoom_array = np.array(zoom_img)
-    print(f'The shape of image is: (400, 400, 1) or (400, 400)')
+    print(f'The shape of image is: ({zoom_size}, 400, 1) or (400, 400)')
     print(zoom_array)
     rotate_img = zoom_img.rotate(90)
     print('New shape after Transpose: (400 ,400)')
     rotate_array = np.array(rotate_img)
     print(rotate_array)
-    fig = plt.figure()
-    plt.imshow(rotate_array)
+    plt.figure()
+    plt.imshow(rotate_array, cmap="gray")
     plt.show()
 
+
 def main():
+    """
+    main function
+    """
     rotate()
 
 
