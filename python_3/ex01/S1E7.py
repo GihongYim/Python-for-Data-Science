445f9a96a47f9e456f147d2bb14aa7fedaa8939f
Gihong Yim <gyim@c1r2s7.42seoul.kr>
2023-10-25 21:01:08 +0900

    python_3 docstring checking

--- python_3/ex01/S1E7.py
+++ python_3/ex01/S1E7.py
@@ -2,29 +2,52 @@ from S1E9 import Character
 
 
 class Baratheon(Character):
-    """Representing the Baratheon family."""
-    # your code here
+    """
+        Baratheon Class inhereted from Character class
+    """
 
     def __init__(self, first_name, is_alive=True):
-        # your code here
+        """
+        Constructor Baratheon lcass
+
+        Args:
+            first_name (string): first_name
+            is_alive (bool, optional): set Character alive status true. Defaults to True.
+        """
         super().__init__(first_name=first_name, is_alive=is_alive)
         self.family_name = 'Baratheon'
         self.eyes = 'brown'
         self.hairs = 'dark'
     
     def die(self):
+        """
+        Set Character class instance is_alive status False
+        """
         self.is_alive = False
         # return not self.is_alive
     
     def __str__(self):
+        """
+            return string information about Baratheon attribute
+        
+        Returns:
+            string: "{}, {}, {}".format(self.family_name, self.eyes, self.hairs)
+        """
         return "{}, {}, {}".format(self.family_name, self.eyes, self.hairs)
     
     def __repr__(self):
+        """
+        representation for Baratheon Class instance
+        Returns:
+            string: "Vector: ('{}', '{}', '{}')".format(self.family_name, self.eyes, self.hairs)
+        """
         return "Vector: ('{}', '{}', '{}')".format(self.family_name, self.eyes, self.hairs)
 
 
 class Lannister(Character):
-    """docstring for Lannister Class"""
+    """
+    Lannister class inhereted from Character
+    """
     
     def __init__(self, first_name, is_alive=True):
         # your code here
@@ -34,18 +57,46 @@ class Lannister(Character):
         self.hairs = 'light'
     
     def die(self):
-        """Lannister die"""
-        return not self.is_alive
+        """
+           Set Character class instance is_alive status False
+
+        Returns:
+            : _description_
+        """
+        self.is_alive = False
+        # return not self.is_alive
     
     def __str__(self):
+        """
+            return string information about Lannister attribute
+        
+        Returns:
+            string: "{}, {}, {}".format(self.family_name, self.eyes, self.hairs)
+        """
+        
         return "{}, {}, {}".format(self.family_name, self.eyes, self.hairs)
     
     def __repr__(self):
+        """
+        representation for Lannister Class instance
+        Returns:
+            string: "Vector: ('{}', '{}', '{}')".format(self.family_name, self.eyes, self.hairs)
+        """
+        
         return "Vector: ('{}', '{}', '{}')".format(self.family_name, self.eyes, self.hairs)
 
     # decorator
     @classmethod
     def create_lannister(self, first_name, is_alive):
-        """create_lannister : make new lannister"""
+        """
+        
+
+        Args:
+            first_name (string): Lannister's first_name
+            is_alive (bool): set Lannister 
+
+        Returns:
+            Lannister instance: return Lannister instance
+        """
         # your code here
         return Lannister(first_name, is_alive)
