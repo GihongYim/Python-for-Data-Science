23770512bb7fca630dfa521b5cc444b882269c2b
Gihong Yim <gyim@c2r8s1.42seoul.kr>
2024-01-23 21:03:39 +0900

    ex06 not finished

--- python_0/ex06/ft_filter.py
+++ python_0/ex06/ft_filter.py
@@ -1,21 +1,12 @@
-def ft_filter(function, iterable):
-    """
-ft_filter(function or None, iterable) --> filter object
+def ft_filter(function, iterable) -> filter:
+    """ft_filter(function or None, iterable) --> filter object
 
 Return an iterator yielding those items of iterable for which function(item)
 is true. If function is None, return the items that are true.
     """
-    result = []
-    if function is None:
-        result = [x for x in iterable if x is True]
-    else:
-        result = [x for x in iterable if function(x) is True]
-    return result
-
-
-def main():
     pass
 
+# if __name__ == "__main__":
+#     pass
 
-if __name__ == "__main__":
-    main()
+# filter()
