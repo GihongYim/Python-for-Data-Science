9c505cfac0905addf7a4dc521c6beab8d58f599e
root <root@DESKTOP-HDUL0BI>
2024-01-24 22:10:24 +0900

    ft_filter.py  complete

--- python_0/ex06/ft_filter.py
+++ python_0/ex06/ft_filter.py
@@ -4,9 +4,26 @@ def ft_filter(function, iterable) -> filter:
 Return an iterator yielding those items of iterable for which function(item)
 is true. If function is None, return the items that are true.
     """
-    pass
+    if function == None:
+        result = [item for item in iterable if iterable]
+    else:
+        result = [item for item in iterable if function(item)]
+    return iter(result)
+    
+    
+def func(x):
+    if x == 3:
+        return True
+    else:
+        return False
 
-# if __name__ == "__main__":
-#     pass
 
-# filter()
+if __name__ == "__main__":
+    # print(ft_filter.__doc__)
+    arr = [1, 2, 3, 4, 5]
+    arr2 = ft_filter(func, arr)
+    arr3 = filter(func, arr)
+    for element in arr2:
+        print(element)
+    for element in arr3:
+        print(element)
