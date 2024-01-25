21c491e3b41abc73e739a2d93d94554e157a0a74
Gihong Yim <gyim@c2r8s1.42seoul.kr>
2024-01-25 16:04:10 +0900

    Loading.py

--- python_0/ex08/Loading.py
+++ python_0/ex08/Loading.py
@@ -1,19 +1,40 @@
+import os
+from time import sleep
+
+
 def ft_tqdm(total):
-    """ ft_tqdm(generator) -> generator result"""
-    line_length = 109
+    """_summary_
+        show total's process
+
+    Args:
+        total (_type_): iterable or list etc...
+
+    Yields:
+        _type_: total's iterator result
+    """
+
+    line_length = os.get_terminal_size().columns
+    cnt = 1
     for i in total:
         percent = (i + 1) * 100 // len(total)
+        # print(percent)
         percent_str = f'{percent:3}%'
-        bar = "█" * ((i + 1) * (line_length) // len(total))
-        space = " " * (line_length - len(bar))
+        remain_length = line_length - len(percent_str)
+        curr_over_total = f' {i}/{len(total)}'
+        remain_length = remain_length - len(curr_over_total) - 2
+        bar = "█" * int((cnt / len(total)) * remain_length)
+        space = " " * (remain_length - len(bar))
         process_bar = f'|{bar + space}|'
-        curr_over_total = f'{i}/{len(total)}'
-        print(f"\r{percent_str}{process_bar} {curr_over_total}", end="")
+        print(f"\r{percent_str}{process_bar}{curr_over_total}", end="")
+        cnt += 1
         yield i
 
 
 def main():
-    pass
+    """main function for testing ft_tqdm
+    """
+    for i in ft_tqdm(range(10000)):
+        sleep(0.01)
 
 
 if __name__ == "__main__":
