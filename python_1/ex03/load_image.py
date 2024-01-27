1470f0c90bf83cee72c42386e6988babde869af4
root <root@DESKTOP-HDUL0BI>
2024-01-27 11:13:42 +0900

    rollback load image

--- python_1/ex03/load_image.py
+++ python_1/ex03/load_image.py
@@ -2,21 +2,29 @@ import numpy as np
 from PIL import Image
 
 
-def ft_load(path: str) -> np.array:
+def ft_load(filename):
     """_summary_
-
+        load image and print RGB value for each pixel
     Args:
-        path (str): image filepath
-
-    Returns:
-        np.array: image pixel array
+        filename (_type_): valid for Image.open() function
+    Return:
+        img: np.array
     """
-    try:
-        img = Image.open(path)
-    except Exception as e:
-        print(f"{e.__class__.__name__}: {e}")
-        exit()
-    x = np.array(img)
-    print(f"The shape of image is: {x.shape}")
-    print(x)
-    return x
+
+    img = Image.open(filename)
+    array = np.array(img)
+    print(f'The shape of image is: {array.shape}')
+    print(array)
+    return array
+
+
+def main():
+    """
+    _summary_
+    test function for ft_load
+    """
+    ft_load("animal.jpeg")
+
+
+if __name__ == "__main__":
+    main()
