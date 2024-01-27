fae1e893b3ce8e5ab98355be749df6f636775f33
root <root@DESKTOP-HDUL0BI>
2024-01-27 11:11:47 +0900

    python_01 ex02

--- python_1/ex02/load_image.py
+++ python_1/ex02/load_image.py
@@ -2,29 +2,21 @@ import numpy as np
 from PIL import Image
 
 
-def ft_load(filename):
+def ft_load(path: str) -> np.array:
     """_summary_
-        load image and print RGB value for each pixel
+
     Args:
-        filename (_type_): valid for Image.open() function
-    Return:
-        img: np.array
+        path (str): image filepath
+
+    Returns:
+        np.array: image pixel array
     """
-
-    img = Image.open(filename)
-    array = np.array(img)
-    print(f'The shape of image is: {array.shape}')
-    print(array)
-    return array
-
-
-def main():
-    """
-    _summary_
-    test function for ft_load
-    """
-    ft_load("animal.jpeg")
-
-
-if __name__ == "__main__":
-    main()
+    try:
+        img = Image.open(path)
+    except Exception as e:
+        print(f"{e.__class__.__name__}: {e}")
+        exit()
+    x = np.array(img)
+    print(f"The shape of image is: {x.shape}")
+    print(x)
+    return x
