37c14fb2eebae0af25e68f87ee550ef922e02292
GihongYim <56685220+GihongYim@users.noreply.github.com>
2024-01-29 22:32:19 +0900

    asdf

--- python_3/ex03/ft_calculator.py
+++ python_3/ex03/ft_calculator.py
@@ -1,35 +1,52 @@
 class calculator:
-    """Representing Calculator class """
-    # your code here
+    """ calculator class
+    """
     
     def __init__(self, object):
-        # your code here
+        """_summary_
+
+        Args:
+            object (_type_): _description_
+        """
         self.object = object
         pass
     
     def __add__(self, object) -> None:
-        # your code here
+        """_summary_
+        """
         self.object = list(map(lambda x: x + object, self.object))
         print(self.object)
-        pass
+        return self.object
 
     def __mul__(self, object) -> None:
-        # your code here
+        """_summary_
+
+        Args:
+            object (_type_): _description_
+        """
         self.object = list(map(lambda x: x * object, self.object))
         print(self.object)
-        pass
+        return self.object
 
     def __sub__(self, object) -> None:
-        # your code here
+        """_summary_
+
+        Args:
+            object (_type_): _description_
+        """
         self.object = list(map(lambda x: x - object, self.object))
         print(self.object)
-        pass
+        return self.object
 
     def __truediv__(self, object) -> None:
-        # your code here
+        """_summary_
+
+        Args:
+            object (_type_): _description_
+        """
         try:
             self.object = list(map(lambda x: x / object, self.object))
             print(self.object)
         except ZeroDivisionError as e:
             print(f"{e} : not dividev by zero")
-        pass
+        return self.object
