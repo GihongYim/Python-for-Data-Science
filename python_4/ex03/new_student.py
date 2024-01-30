ad6584f0fc0db8cac2d01e785724627de34fb843
Gihong Yim <gyim@c2r8s1.42seoul.kr>
2024-01-30 16:38:15 +0900

    new_student.py empty stirng error case solved

--- python_4/ex03/new_student.py
+++ python_4/ex03/new_student.py
@@ -4,29 +4,54 @@ from dataclasses import dataclass, field
 
 
 def generate_id() -> str:
+    """generate_id() -> str
+
+    Returns:
+        str: random_id
+    """
     return "".join(random.choices(string.ascii_lowercase, k=15))
 
 
 @dataclass()
 class Student:
-    """Representing Student Class"""
+    """
+    class Student
     name: str
     surname: str
     active: bool
     login: str = field(kw_only=True)
     id: str = field(kw_only=True)
-    
+    """
+    name: str
+    surname: str
+    active: bool
+    login: str = field(kw_only=True)
+    id: str = field(kw_only=True)
+
     def __init__(self, name: str, surname: str, active: bool = True):
+        """__init__
+
+        Args:
+            name (str): student name
+            surname (str): surname of student
+            active (bool, optional): student active. Defaults to True.
+        """
+        if name == "":
+            print("Error: name is empty string")
+            exit()
+        if surname == "":
+            print("Error: surname is empty string")
+            exit()
         self.name = name
         self.surname = surname
         self.active = active
         self.login = name[0] + surname
         self.id = generate_id()
-    
+
 
 def main():
     pass
-   
+
 
 if __name__ == "__main__":
     main()
