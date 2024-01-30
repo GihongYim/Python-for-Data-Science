18a9fc05c53cc32d7426ed1df1dbf8413d96b91b
Gihong Yim <gyim@c2r8s1.42seoul.kr>
2024-01-30 15:44:02 +0900

    in_out.py count=0 => count=None

--- python_4/ex01/in_out.py
+++ python_4/ex01/in_out.py
@@ -1,19 +1,47 @@
 def square(x: int | float) -> int | float:
-    # your code here
+    """square(x: int | float) -> int | float
+
+    Args:
+        x (int | float): base of power
+
+    Returns:
+        int | float: x square 2
+    """
     return x ** 2
 
 
 def pow(x: int | float) -> int | float:
-    # your code here
+    """pow(x: int | float) -> int | float
+
+    Args:
+        x (int | float): base of power
+
+    Returns:
+        int | float: x power of x
+    """
     return x ** x
 
 
 def outer(x: int | float, function) -> object:
-    count = 0
+    """outer(x: int | float, function) -> object
+
+    Args:
+        x (int | float): function input
+        function (_type_): return function store x
+
+    Returns:
+        object: function calculate power
+    """
+    count = None
     
     def inner() -> float:
+        """inner() -> float
+
+        Returns:
+            float: calculate function()
+        """
         nonlocal count
-        if count == 0:
+        if count == None:
             count = function(x)
         else:
             count = function(count)
