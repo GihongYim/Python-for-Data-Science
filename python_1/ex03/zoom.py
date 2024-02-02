68685595336a532486ac80375138162d8ba1e976
Gihong Yim <gyim@c2r8s1.42seoul.kr>
2024-02-02 18:00:13 +0900

    zoom.py add RGB image check code

--- python_1/ex03/zoom.py
+++ python_1/ex03/zoom.py
@@ -14,13 +14,23 @@ def zoom(filename: str, box: tuple) -> None:
         print information about it
         diaplay it after zooming
     """
+
     array = ft_load(filename)
-    r, g, b = array[:, :, 0], array[:, :, 1], array[:, :, 2]
-    gray_array = 0.2989 * r + 0.5870 * g + 0.1140 * b
-    gray_array = gray_array.astype('uint8')
-    gray_img = Image.fromarray(gray_array)
-    zoom_img = gray_img.crop(box)
-    zoom_array = np.array(zoom_img)
+    # gray image 직접계산
+    try:
+        assert array.ndim == 3 and array.shape[2] == 3, \
+            "image is not RGB image"
+        r, g, b = array[:, :, 0], array[:, :, 1], array[:, :, 2]
+        gray_array = 0.2989 * r + 0.5870 * g + 0.1140 * b
+        gray_array = gray_array.astype('uint8')
+        # # gray image PIL 모듈 사용
+        # gray_array = np.array(Image.fromarray(array).convert('L'))
+        gray_img = Image.fromarray(gray_array)
+        zoom_img = gray_img.crop(box)
+        zoom_array = np.array(zoom_img)
+    except Exception as e:
+        print(f"{e.__class__.__name__}: {e}")
+        exit(1)
 
     print(f'New shape after slicing: {zoom_array.shape}')
     print(zoom_array)
