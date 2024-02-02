457ae656840056aa16047c2f32d8844180bcce7e
Gihong Yim <gyim@c2r8s1.42seoul.kr>
2024-02-02 12:35:27 +0900

    python_0 ex03 failure in evaluation changed

--- python_0/ex03/NULL_not_found.py
+++ python_0/ex03/NULL_not_found.py
@@ -1,15 +1,15 @@
 def NULL_not_found(object: any) -> int:
     # your code here
-    if isinstance(object, type(None)):
-        print(f"Nothing: {object} {type(None)}")
-    elif isinstance(object, float):
-        print(f"Cheese: {object} {float}")
-    elif isinstance(object, int):
-        print(f"Zero: {object} {int}")
-    elif isinstance(object, str):
-        print(f"Empty: {object} {str}")
-    elif isinstance(object, bool):
-        print(f"Fake: {object} {bool}")
+    if object is None:
+        print(f"Nothing: {object} {type(object)}")
+    elif isinstance(object, float) and object != float('NaN'):
+        print(f"Cheese: {object} {type(object)}")
+    elif object == 0:
+        print(f"Zero: {object} {type(object)}")
+    elif object == '':
+        print(f"Empty: {object} {type(object)}")
+    elif object is False:
+        print(f"Fake: {object} {type(object)}")
     else:
         print("Type not Found")
     return 1
