438619307a2e7d94a9fc4419785bdaba61356605
Gihong Yim <gyim@c2r8s1.42seoul.kr>
2024-02-02 16:23:27 +0900

    family[start:end] index error handled

--- python_1/ex01/array2D.py
+++ python_1/ex01/array2D.py
@@ -18,19 +18,19 @@ def slice_me(family: list, start: int, end: int) -> list:
     array = []
     try:
         assert type(family[0]) is list, \
-            "AssertionError: family is not 2-dimensional array"
+            "family is not 2-dimensional array"
         if start >= len(family) or -start > len(family):
-            raise IndexError("IndexError: start list index out of range")
+            raise IndexError("start list index out of range")
         if end >= len(family) or -end > len(family):
-            raise IndexError("IndexError: end list index out of range")
+            raise IndexError("end list index out of range")
         row = len(family)
         column = len(family[0])
         assert all(column == len(col) for col in family), \
             "column length are not equal"
-        print(f"My shape is : ({row}, {column})")
         array = family[start:end]
         new_row = len(array)
         new_column = len(array[0])
+        print(f"My shape is : ({row}, {column})")
         print(f"My new shape is : ({new_row}, {new_column})")
     except Exception as e:
         print(f"{e.__class__.__name__}: {e}")
